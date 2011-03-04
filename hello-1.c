002acde956cabbbc9a7b2397d924c4cd03684f21
Kim Taeho <dhvkcy21@nate.com>
2011-03-05 02:41:43 +0900

    edit printk 'hello world'

--- hello-1.c
+++ hello-1.c
@@ -1,2 +1,14 @@
-#include <stdio.h>
+#include <linux/module.h>
+#include <linux.kernel.h>
+
+int init_module (void)
+{
+    printk(KERN_INFO "Hello world 1.\n");
+    return 0;
+}
+
+void cleanup_module (void)
+{
+    printk(KERN_INFO "Goodbye world 1.\n");
+}
 
