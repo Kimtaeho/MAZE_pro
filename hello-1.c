677b471fd182f264dedcaa9a99fbc08be34ebc89
Kim Taeho <dhvkcy21@nate.com>
2011-03-08 02:44:05 +0900

    Make 'module_init() & module_exit()' macros.

--- hello-1.c
+++ hello-1.c
@@ -1,14 +1,18 @@
 #include <linux/module.h>
 #include <linux.kernel.h>
+#include <linux/init.h>
 
-int init_module (void)
+static int __init hello_2_init(void)
 {
-    printk(KERN_INFO "Hello world 1.\n");
+    printk(KERN_INFO "Hello, world 2\n");
     return 0;
 }
 
-void cleanup_module (void)
+static void __exit hello_2_exit(void)
 {
-    printk(KERN_INFO "Goodbye world 1.\n");
+    printk(KERN_INFO "Goodbye, world 2\n");
 }
+
+module_init(hello_2_init);
+module_exit(hello_2_exit);
 
